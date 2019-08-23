8d069919ea7e5bf3fbeee66648f1fe911291b05b
Aniruddha Chatterjee <48330897+ruddha2001@users.noreply.github.com>
2019-08-23 22:41:23 +0530

    FAULTY: Android API
    Android App is not able to decode the JSON Data

---
+++ androidapi.php
@@ -0,0 +1,26 @@
+<?php
+
+    header("Access-Control-Allow-Origin: *");//Enables Cross-Origin Resource Sharing (CORS)
+
+    require "connect.php";
+
+    /**
+     * Name- callFunc
+     * Parameters- none
+     */
+    function callFunc() {
+
+        $query = "SELECT * FROM datastorage ORDER BY id DESC LIMIT 1";
+
+        $result = mysqli_query($GLOBALS['conn'],$query);
+
+        $row = mysqli_fetch_assoc($result);
+
+        $row['sensord']=str_replace(array("\n", "\r"), '', $row['sensord']);//Removes undesired newlines
+
+        $var=json_encode($row);
+
+        echo $var;//Echoes a JSON object
+    }
+    callFunc();
+?>
